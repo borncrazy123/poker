fb3b5bf67a590d0711ed4f006dfcaffcdcdc9705
borncrazy123 <borncrazy123@163.com>
2022-10-19 22:07:52 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,38 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1666186646051_3583';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // 添加 view 配置
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
---
+++ config/plugin.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+module.exports = {
+  // had enabled by egg
+  // static: {
+  //   enable: true,
+  // }
+
+  nunjucks: {
+    enable: true,
+    package: 'egg-view-nunjucks',
+  }
+
+};
