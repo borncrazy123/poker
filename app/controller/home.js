2d26a897d853e67359ec164e86a8caa7c555aca7
cc <borncrazy123@163.com>
2022-10-21 09:08:12 +0800

    init6

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,8 +4,22 @@ const { Controller } = require('egg');
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this;
-    ctx.body = 'hi, egg........';
+    const { ctx, app } = this;
+    // ctx.body = 'hi, egg........';
+
+    const results = await app.mysql.query('select * from tb_1');
+    console.log('results:', results);
+
+    const dataList = {
+      list: [
+        { id: 1, title: 'this is news 1', url: '/news/1' },
+        { id: 2, title: 'this is news 2', url: '/news/2' },
+      ],
+      data: results
+    };
+    
+    await ctx.render('home/home.tpl', dataList);
+
   }
 }
 
